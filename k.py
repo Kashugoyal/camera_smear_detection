184ff4bf9bdb7774ef780a94f7683c832c805fed
Kashish <kashishgoyal2019@u.northwestern.edu>
2018-04-18 20:34:25 -0500

    added for sum

--- k.py
+++ k.py
@@ -28,8 +28,8 @@ def average_images(path,n):
   # for image in images:
     if i<n:
       # avg = cv2.add(cv2.imread(image_path,1)/n , avg)
-      img = 255 - cv2.imread(image_path,0)
-      # img = cv2.imread(image_path,0)
+      # img = 255 - cv2.imread(image_path,0)
+      img = cv2.imread(image_path,0)
       blur = cv2.blur(img, (5,5),0)
       cv2.accumulateWeighted(img,avg,0.01)
       res1 = cv2.convertScaleAbs(avg)
@@ -46,14 +46,14 @@ def display(obj):
   # cv2.imshow("image", cv2.subtract(images[49],images[48]))
   cv2.imshow('image', obj)
   cv2.waitKey(0)                   # Wait for a keystroke in the window
-  cv2.destroyAllWindows()
+  # cv2.destroyAllWindows()
 
 
 def main():
 
   # path = '/home/suhailps/Documents/Assignments/Spring_18/Geospatial/Assignment1/sample_drive/cam_0'
   # Please add the images files
-  path = '/home/kashish/Downloads/sample_drive/cam_5'
+  path = '/home/kashish/Downloads/sample_drive/cam_0'
   num_images = len(glob.glob(path + "/*.jpg"))*1.0
   read_images(path)
 
@@ -67,14 +67,14 @@ def main():
   display(avg1)
   avg1 = cv2.GaussianBlur(avg1, (7,7),0)
   display(avg1)
-  clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+  clahe = cv2.createCLAHE(clipLimit=20.0, tileGridSize=(6,6))
   cl1 = clahe.apply(avg1)
   display(cl1)
-  ret,th1 = cv2.threshold(avg1,100,255,cv2.THRESH_BINARY)
+  # ret,th1 = cv2.threshold(avg1,100,255,cv2.THRESH_BINARY)
   # th1 = cv2.threshold(avg1, 130,255,cv2.THRESH_BINARY)
 
-  display(th1)
-  display(cv2.Canny(th1,100,200))
+  # display(th1)
+  # display(cv2.Canny(th1,100,200))
 
   # display(res1)
   # display(laplacian*100)
