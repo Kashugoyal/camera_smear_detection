d120052f09e78e27d08bbb108b8616f42eb6a818
Kashish <kashishgoyal2019@u.northwestern.edu>
2018-04-19 09:39:51 -0500

    Updated

--- k.py
+++ k.py
@@ -28,10 +28,10 @@ def average_images(path,n):
   # for image in images:
     if i<n:
       # avg = cv2.add(cv2.imread(image_path,1)/n , avg)
-      # img = 255 - cv2.imread(image_path,0)
-      img = cv2.imread(image_path,0)
-      blur = cv2.blur(img, (5,5),0)
-      cv2.accumulateWeighted(img,avg,0.01)
+      img = 255 - cv2.imread(image_path,0)
+      # img = cv2.imread(image_path,0)
+      blur = cv2.blur(img, (7,7))
+      cv2.accumulateWeighted(blur,avg,0.01)
       res1 = cv2.convertScaleAbs(avg)
     else:
       break
@@ -53,23 +53,24 @@ def main():
 
   # path = '/home/suhailps/Documents/Assignments/Spring_18/Geospatial/Assignment1/sample_drive/cam_0'
   # Please add the images files
-  path = '/home/kashish/Downloads/sample_drive/cam_0'
+  path = '/home/kashish/Downloads/sample_drive/cam_5'
   num_images = len(glob.glob(path + "/*.jpg"))*1.0
   read_images(path)
 
   # avg = np.zeros(images[0].shape,np.float)     #float for accumulated function, uint8 for normal addition
 
   # cv2.accumulate(images[1]/100, avg)
-  avg1 = average_images(path,1000.0)
+  avg1 = average_images(path,num_images)
   # res1 = cv2.convertScaleAbs(avg1)
   # laplacian = cv2.Laplacian(avg1,cv2.CV_64F)
   # avg2 = average_images(path,110)
   display(avg1)
-  avg1 = cv2.GaussianBlur(avg1, (7,7),0)
+  avg1 = cv2.blur(avg1, (5,5))
   display(avg1)
   clahe = cv2.createCLAHE(clipLimit=20.0, tileGridSize=(6,6))
   cl1 = clahe.apply(avg1)
   display(cl1)
+  cv2.imwrite('outcam5.jpg',cl1)
   # ret,th1 = cv2.threshold(avg1,100,255,cv2.THRESH_BINARY)
   # th1 = cv2.threshold(avg1, 130,255,cv2.THRESH_BINARY)
 
--- l.py
+++ l.py
@@ -28,8 +28,8 @@ def average_images(path,n):
   # for image in images:
     if i<n:
       # avg = cv2.add(cv2.imread(image_path,1)/n , avg)
-      # img = 255 - cv2.imread(image_path,0)
-      img = cv2.imread(image_path,0)
+      img = 255 - cv2.imread(image_path,0)
+      # img = cv2.imread(image_path,0)
       blur = cv2.blur(img, (5,5),0)
       print i
       if np.sum(img)<507000000 and np.sum(img)>410000000:
@@ -72,7 +72,7 @@ def main():
 
   # path = '/home/suhailps/Documents/Assignments/Spring_18/Geospatial/Assignment1/sample_drive/cam_0'
   # Please add the images files
-  path = '/home/kashish/Downloads/sample_drive/cam_0'
+  path = '/home/kashish/Downloads/sample_drive/cam_5'
   num_images = len(glob.glob(path + "/*.jpg"))*1.0
   read_images(path)
 
@@ -80,9 +80,8 @@ def main():
 
 
   # avg = np.zeros(images[0].shape,np.float)     #float for accumulated function, uint8 for normal addition
-
   # cv2.accumulate(images[1]/100, avg)
-  avg1 = average_images(path,4000.0)
+  avg1 = average_images(path,num_images)
   # res1 = cv2.convertScaleAbs(avg1)
   # laplacian = cv2.Laplacian(avg1,cv2.CV_64F)
   # avg2 = average_images(path,110)
@@ -92,23 +91,11 @@ def main():
   clahe = cv2.createCLAHE(clipLimit=20.0, tileGridSize=(6,6))
   cl1 = clahe.apply(avg1)
   display(cl1)
+  th1 = cv2.adaptiveThreshold(cl1,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+  display(th1)
+
   # ret,th1 = cv2.threshold(avg1,100,255,cv2.THRESH_BINARY)
   # th1 = cv2.threshold(avg1, 130,255,cv2.THRESH_BINARY)
-
-  # display(th1)
-  # display(cv2.Canny(th1,100,200))
-
-  # display(res1)
-  # display(laplacian*100)
-  # display(avg2)
-  # display(cv2.subtract(avg2, avg1))
-  # display(cv2.absdiff(images[2] , images[1]))
-  # display(cv2.absdiff(images[3] , images[2]))
-
-
-  # display(cv2.subtract(images[5],images[4]))
-  # display(cv2.subtract(images[7],images[6]))
-
 
 
 if __name__ == "__main__":
