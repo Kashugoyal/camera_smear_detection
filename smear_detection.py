ede3fe347eab12e49c6421d3769752aed7e97c8f
Kashish <kashishgoyal2019@u.northwestern.edu>
2018-04-19 20:42:41 -0500

    Added threshold function

--- smear_detection.py
+++ smear_detection.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 import numpy as np
 import cv2
 import glob
 import sys
 
-#function to fingd the Average image
+#function to find the Average image
 def average_images(path,n):
 
   first_img=cv2.imread(glob.glob(path + '/*.jpg')[0],0)
@@ -22,48 +23,52 @@ def average_images(path,n):
     #For testing
     if i>500:
         break
-
   return res
+
+
+
+
 
 def subtract(path,n):
 
-  first_img=cv2.imread(glob.glob(path + '/*.jpg')[0],0)
-  avg =np.zeros(first_img.shape,np.float)     #float for accumulated function, uint8 for normal addition
-  diff =np.zeros(first_img.shape,np.float)
-  sum =np.zeros(first_img.shape,np.float)
-  i =0
+    first_img=cv2.imread(glob.glob(path + '/*.jpg')[0],0)
+    avg =np.zeros(first_img.shape,np.float)     #float for accumulated function, uint8 for normal addition
+    diff =np.zeros(first_img.shape,np.float)
+    sum =np.zeros(first_img.shape,np.float)
+    i =0
 
-  for image_path in glob.glob(path + '/*.jpg'):
-    print i#For getting the status in terminal
+    for image_path in glob.glob(path + '/*.jpg'):
+        print i#For getting the status in terminal
 
-    if i%2==0:
-        img1 =  cv2.imread(image_path,0)
-    else:
-        img2 =  cv2.imread(image_path,0)
+        if i%2==0:
+            img1 =  cv2.imread(image_path,0)
+        else:
+            img2 =  cv2.imread(image_path,0)
 
-        diff=cv2.subtract(img1,img2)
-        if i==1:
-            sum=diff*0.00001
-        # display(diff,'difference')
+            diff=cv2.subtract(img1,img2)
+            if i==1:
+                sum=diff*0.00001
+            # display(diff,'difference')
 
-    cv2.accumulateWeighted(diff,avg,0.01)
-    res = cv2.convertScaleAbs(avg)
-    if i>1:
-        sum=cv2.add(sum,diff*0.00001)
+        cv2.accumulateWeighted(diff,avg,0.01)
+        res = cv2.convertScaleAbs(avg)
+        if i>1:
+            sum=cv2.add(sum,diff*0.00001)
 
-    display(sum,'sum')
-    cv2.imwrite('output2.jpg',res)
+        display(sum,'res')
+        i+=1
+        #For testing
+        # if i>500:
+        #     break
+    cv2.imwrite('output2.jpg',sum)
+    return sum
 
-    i=i+1
-    #For testing
-    # if i>500:
-    #     break
 
 
-  display(res,'Average')
 
-  cv2.imwrite('output2.jpg',res)
-  return res
+
+
+
 
 #Fucntion to display an image
 def display(image,window_name):
@@ -71,31 +76,56 @@ def display(image,window_name):
   cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
   cv2.resizeWindow(window_name, 600,600)
   cv2.imshow(window_name, image)
-  cv2.waitKey(10)                   # Wait for a keystroke in the window
+  cv2.waitKey(0)                   # Wait for a keystroke in the window
   # cv2.destroyAllWindows()
 
+
+
+
+def threshold(path):
+    i=0
+    for image_path in glob.glob(path + '/*.png'):
+        print i, image_path
+        img = cv2.imread(image_path,0)
+        mean = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,51,0)
+        gaussian = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,51,0)
+        display(mean, 'mean')
+        display(gaussian,'gaussian')
+        cv2.destroyAllWindows()
+        i+=1
+
+    # return mean , gaussian
+
+
+
+
+
+
 def main():
-    arguments = sys.argv[1:]
+    # arguments = sys.argv[1:]
 
-
-
-    #Checking if pathis entered
+    #Checking if path is entered
     if len(sys.argv) < 2:
-        print 'Enter path to input camera forder'
-        sys.exit(1)
-
-
-    #path = '/home/suhailps/Documents/Assignments/Spring_18/Geospatial/Assignment1/sample_drive/cam_0'
-    path =sys.argv[1]
+        path = '/home/kashish/Downloads/sample_drive/cam_5'
+        print 'No path given, using defaut path'
+        # sys.exit(0)
+    else:
+        path =sys.argv[1]
 
     #Number of images in the folder
     n_of_images=glob.glob(path + '/*.jpg')
 
+    threshold(path)
+    sys.exit(0)
+
 
     #New approach
-    # subtract(path,n_of_images)
+    sum = subtract(path,n_of_images)
+    cv2.imshow('sum', sum)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    # sys.exit(1)
+    sys.exit(1)
     #New approach
 
     #Finding the average image and
@@ -108,8 +138,8 @@ def main():
 
     clahe = cv2.createCLAHE(clipLimit=20.0, tileGridSize=(6,6))
     cl1 = clahe.apply(avg_blurred)
-    display(cl1,'After CLAHE')
-
+    display(cl1,'After CLAHE'
+)
     ret,th1 = cv2.threshold(cl1, 50,255,cv2.THRESH_BINARY)
     display(th1,'Threshold img')
 
@@ -144,5 +174,5 @@ def main():
     #     else:
     #         contours.remove(c)
 
-
-main()
+if __name__=="__main__":
+    main()
