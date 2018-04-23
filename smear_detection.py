109cc79b934425cc8ce64fd03aade57ca5879fd6
Kashish <kashishgoyal2019@u.northwestern.edu>
2018-04-23 14:22:17 -0500

    Final Commit

--- smear_detection.py
+++ smear_detection.py
@@ -7,57 +7,37 @@ import scipy.ndimage as scpy
 from skimage.filter import threshold_adaptive
 
 
-
+# calculates of the average of the obtained subtracted images 
 def find_average(path,n):
 
-    #Reading the first image for getting the shape for initialisation
+    # Reading the first image for getting the shape for initialization
     first_img=cv2.imread(glob.glob(path + '/*.jpg')[0],0)
-
-    #initialising blank images
+    # initialising blank images
     diff =np.zeros(first_img.shape,np.float)
     avg_image =np.zeros(first_img.shape,np.float)
-
     i =0
-    #Looping through all images
+    print 'Processing a total of {} images'.format(n)
+    # Looping through all images
     for image_path in glob.glob(path + '/*.jpg'):
-        print i#For getting the status in terminal
-
         if i%2==0:
             img1 =  cv2.imread(image_path,0)
         else:
             img2 =  cv2.imread(image_path,0)
-
-            #Difference of consecutive images found
+            # Difference of consecutive images found
             diff=cv2.subtract(img1,img2)
-
-            # #For
-            # if i==1:
-            #     avg_image=diff*0.00001
-            # # display(diff,'difference')
-
-        # cv2.accumulateWeighted(diff,avg,0.01)
-        # res = cv2.convertScaleAbs(avg)
-        # if i>1:
         avg_image=cv2.add(avg_image,diff*0.00001)
-
         display(avg_image,'Average',1)
         # cv2.imwrite('./average.jpg',255*avg_image)
-
+        print 'Processing Image number' ,  i  #For getting the status in terminal
         i+=1
-        #For testing
-        # if i>500:
-        #     break
     display(avg_image,'Average',0)
     # Saving the image by converting to 255 scale
     cv2.imwrite('average.jpg',255*avg_image)
-    print 'Saved average'
+    print 'The average image has been saved in the current directory.'
     return 255*avg_image
 
 
-
-
-
-#Fucntion to display an image
+#Function to display an image
 def display(image,window_name,time):
 
   cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
@@ -66,6 +46,8 @@ def display(image,window_name,time):
   cv2.waitKey(time)                   # Wait for a keystroke in the window
 
 
+
+# another approach
 def threshold2(path):
     i=0
     for image_path in glob.glob(path + '/*.png'):
@@ -92,15 +74,19 @@ def threshold2(path):
         i+=1
 
 
+
+# post processing of obtained averages
 def threshold(image):
 
     gaussian_image = scpy.gaussian_filter(image, (10,10))
-    display(gaussian_image/255,'gaussian_image',0)
+    display(gaussian_image,'gaussian_image',0)
+    clahe = cv2.createCLAHE(clipLimit=15.0, tileGridSize=(6,6))
+    cl1 = clahe.apply(gaussian_image)
+    display(cl1,'clahe',0)
 
-    threshold_image = threshold_adaptive(gaussian_image, 255, offset = 10)
+    threshold_image = threshold_adaptive(cl1, 255, offset = 10)
     threshold_average_image = threshold_image.astype(np.uint8) * 255
     display(threshold_average_image,'threshold_average_image',0)
-    # ret,th1 = cv2.threshold(img,180,255,cv2.THRESH_BINARY)
 
     image, contours, hier = cv2.findContours(threshold_average_image, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)#, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     smears=[]
@@ -108,14 +94,21 @@ def threshold(image):
     for cnt in contours:
 
         area = cv2.contourArea(cnt)
-        if area>4000:# and area<7000:
+        if area>6000 and area<25000:  #cam1
+        # if area>25000 and area<26000:  #cam3
+        # if area>6800 and area<7000:  #cam5
+        # if area>6800 and area<7000:  #cam0,2
             print 'area=',area
             smears.append(cnt)
 
     mask = np.zeros(threshold_average_image.shape, np.uint8)
-    cv2.drawContours(mask, smears, -1, (255, 255, 255), -1)
+    cv2.drawContours(mask, smears, - 1, (255, 255, 255), -1)
 
-    cv2.imwrite('mask.jpg',255-mask)
+    # cv2.drawContours(mask, smears, 1, (255, 255, 255), -1)  #cam 1
+    # cv2.drawContours(mask, smears, 2, (255, 255, 255), -1)  #cam 1
+    # cv2.drawContours(mask, smears, 3, (255, 255, 255), -1)  #cam 1
+
+    cv2.imwrite('mask.jpg',mask)
     print 'mask saved'
 
     cv2.destroyAllWindows()
@@ -127,30 +120,28 @@ def main():
 
     #Checking if path is entered
     if len(sys.argv) < 2:
-        path = '/home/kashish/Downloads/sample_drive/cam_5'
+        path = '/home/kashish/Downloads/sample_drive/cam_1'
         print 'No path given, using defaut path'
 
     else:
         path =sys.argv[1]
 
     #Number of images in the folder
-    n_of_images=glob.glob(path + '/*.jpg')
+    n_of_images=len(glob.glob(path + '/*.jpg'))
 
     #New approach
     avg_image = find_average(path,n_of_images)
 
-
-
-    # avg_image =cv2.imread('/home/suhailps/Desktop/Link to Spring_18/Geospatial/Assignment1/Smear_detection/cam5.png',0)
+    # un-comment the followung line to just see the post processing on average images in the direcotry
+    # avg_image =cv2.imread('cam1.png',0)
+    display(avg_image,'input',0)
 
     #Function outputs the mask of the smear
     mask=threshold(avg_image)
     display( mask,'Mask',0)
-
     cv2.destroyAllWindows()
 
 
 if __name__=="__main__":
     main()
-    # threshold2(path)
     # sys.exit(0)
