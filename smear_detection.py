f02137faaa9e21c02e7de7b1c2419f1c4d206c99
Suhail Pallath Sulaiman <suhailsulaiman2018@u.northwestern.edu>
2018-04-23 02:40:49 -0500

    Final code, everything working, averaging, mask and filtering

--- smear_detection.py
+++ smear_detection.py
@@ -10,12 +10,15 @@ from skimage.filter import threshold_adaptive
 
 def find_average(path,n):
 
+    #Reading the first image for getting the shape for initialisation
     first_img=cv2.imread(glob.glob(path + '/*.jpg')[0],0)
-    avg =np.zeros(first_img.shape,np.float)     #float for accumulated function, uint8 for normal addition
+
+    #initialising blank images
     diff =np.zeros(first_img.shape,np.float)
     avg_image =np.zeros(first_img.shape,np.float)
-    i =0
 
+    i =0
+    #Looping through all images
     for image_path in glob.glob(path + '/*.jpg'):
         print i#For getting the status in terminal
 
@@ -24,26 +27,31 @@ def find_average(path,n):
         else:
             img2 =  cv2.imread(image_path,0)
 
+            #Difference of consecutive images found
             diff=cv2.subtract(img1,img2)
-            if i==1:
-                avg_image=diff*0.00001
-            # display(diff,'difference')
 
-        cv2.accumulateWeighted(diff,avg,0.01)
-        res = cv2.convertScaleAbs(avg)
-        if i>1:
-            avg_image=cv2.add(avg_image,diff*0.00001)
+            # #For
+            # if i==1:
+            #     avg_image=diff*0.00001
+            # # display(diff,'difference')
 
-        display(avg_image,'res',1)
-        cv2.imwrite('./average.jpg',avg_image)
+        # cv2.accumulateWeighted(diff,avg,0.01)
+        # res = cv2.convertScaleAbs(avg)
+        # if i>1:
+        avg_image=cv2.add(avg_image,diff*0.00001)
+
+        display(avg_image,'Average',1)
+        # cv2.imwrite('./average.jpg',255*avg_image)
 
         i+=1
         #For testing
-        if i>1000:
-            break
-    cv2.imwrite('average.jpg',avg_image)
+        # if i>500:
+        #     break
+    display(avg_image,'Average',0)
+    # Saving the image by converting to 255 scale
+    cv2.imwrite('average.jpg',255*avg_image)
     print 'Saved average'
-    return avg_image
+    return 255*avg_image
 
 
 
@@ -56,9 +64,6 @@ def display(image,window_name,time):
   cv2.resizeWindow(window_name, 600,600)
   cv2.imshow(window_name, image)
   cv2.waitKey(time)                   # Wait for a keystroke in the window
-  # cv2.destroyAllWindows()
-
-
 
 
 def threshold2(path):
@@ -90,7 +95,7 @@ def threshold2(path):
 def threshold(image):
 
     gaussian_image = scpy.gaussian_filter(image, (10,10))
-    display(gaussian_image,'gaussian_image',0)
+    display(gaussian_image/255,'gaussian_image',0)
 
     threshold_image = threshold_adaptive(gaussian_image, 255, offset = 10)
     threshold_average_image = threshold_image.astype(np.uint8) * 255
@@ -103,18 +108,18 @@ def threshold(image):
     for cnt in contours:
 
         area = cv2.contourArea(cnt)
-        if area>1000:# and area<7000:
+        if area>4000:# and area<7000:
             print 'area=',area
             smears.append(cnt)
 
     mask = np.zeros(threshold_average_image.shape, np.uint8)
     cv2.drawContours(mask, smears, -1, (255, 255, 255), -1)
 
-    cv2.imwrite('mask.jpg',mask)
+    cv2.imwrite('mask.jpg',255-mask)
     print 'mask saved'
 
     cv2.destroyAllWindows()
-    return mask
+    return 255-mask
 
 
 
@@ -133,10 +138,10 @@ def main():
 
     #New approach
     avg_image = find_average(path,n_of_images)
-    cv2.imwrite('mask1.jpg',avg_image)
-    display( avg_image,'Average',0)
 
-    # sum =cv2.imread('/home/suhailps/Desktop/Link to Spring_18/Geospatial/Assignment1/Smear_detection/cam14.png',0)
+
+
+    # avg_image =cv2.imread('/home/suhailps/Desktop/Link to Spring_18/Geospatial/Assignment1/Smear_detection/cam5.png',0)
 
     #Function outputs the mask of the smear
     mask=threshold(avg_image)
