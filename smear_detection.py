18db4a6799c72b35b994f92d5e10d9734a01f549
Suhail Pallath Sulaiman <suhailsulaiman2018@u.northwestern.edu>
2018-04-23 00:13:09 -0500

    Cleaned code with mask creation and filtering unwanted contours using area

--- smear_detection.py
+++ smear_detection.py
@@ -6,37 +6,14 @@ import sys
 import scipy.ndimage as scpy
 from skimage.filter import threshold_adaptive
 
-#function to find the Average image
-def average_images(path,n):
-
-  first_img=cv2.imread(glob.glob(path + '/*.jpg')[0],0)
-  avg =np.zeros(first_img.shape,np.float)     #float for accumulated function, uint8 for normal addition
-  i =0
-
-  for image_path in glob.glob(path + '/*.jpg'):
-    print i#For getting the status in terminal
-    i=i+1
-    img = cv2.imread(image_path,0)
-    blur = cv2.blur(img, (5,5),0)
-    if True:#np.sum(img)<507000000 and np.sum(img)>410000000:
-        cv2.accumulateWeighted(img,avg,0.01)
-        res = cv2.convertScaleAbs(avg)
-
-    #For testing
-    if i>500:
-        break
-  return res
 
 
-
-
-
-def subtract(path,n):
+def find_average(path,n):
 
     first_img=cv2.imread(glob.glob(path + '/*.jpg')[0],0)
     avg =np.zeros(first_img.shape,np.float)     #float for accumulated function, uint8 for normal addition
     diff =np.zeros(first_img.shape,np.float)
-    sum =np.zeros(first_img.shape,np.float)
+    avg_image =np.zeros(first_img.shape,np.float)
     i =0
 
     for image_path in glob.glob(path + '/*.jpg'):
@@ -49,62 +26,38 @@ def subtract(path,n):
 
             diff=cv2.subtract(img1,img2)
             if i==1:
-                sum=diff*0.00001
+                avg_image=diff*0.00001
             # display(diff,'difference')
 
         cv2.accumulateWeighted(diff,avg,0.01)
         res = cv2.convertScaleAbs(avg)
         if i>1:
-            sum=cv2.add(sum,diff*0.00001)
+            avg_image=cv2.add(avg_image,diff*0.00001)
 
-        display(sum,'res')
+        display(avg_image,'res',1)
+        cv2.imwrite('./average.jpg',avg_image)
+
         i+=1
         #For testing
-        # if i>500:
-        #     break
-    cv2.imwrite('output2.jpg',sum)
-    return sum
-
-
-
+        if i>1000:
+            break
+    cv2.imwrite('average.jpg',avg_image)
+    print 'Saved average'
+    return avg_image
 
 
 
 
 
 #Fucntion to display an image
-def display(image,window_name):
+def display(image,window_name,time):
 
   cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
   cv2.resizeWindow(window_name, 600,600)
   cv2.imshow(window_name, image)
-  cv2.waitKey(0)                   # Wait for a keystroke in the window
+  cv2.waitKey(time)                   # Wait for a keystroke in the window
   # cv2.destroyAllWindows()
 
-
-
-
-def threshold(path):
-    i=0
-    for image_path in glob.glob(path + '/*.png'):
-        print i, image_path
-        img = cv2.imread(image_path,0)
-        kernel = np.ones((5,5 ),np.uint8)
-        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-        display(img,'erosion')
-        clahe = cv2.createCLAHE(clipLimit=20.0, tileGridSize=(6,6))
-        img = clahe.apply(img)
-        display(img, 'clahe')
-        # img = cv2.erode(img,kernel,iterations = 1)
-        img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,0)
-        kernel = np.ones((9,9),np.uint8)
-        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-        # gaussian = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,51,0)
-        display(img,'mean')
-        cv2.destroyAllWindows()
-        i+=1
-
-    # return mean , gaussian
 
 
 
@@ -115,129 +68,85 @@ def threshold2(path):
         img = cv2.imread(image_path,0)
 
         gaussian_image = scpy.gaussian_filter(img, (10,10))
-        display(gaussian_image,'gaussian_image')
+        display(gaussian_image,'gaussian_image',0)
 
-        threshold_image = threshold_adaptive(gaussian_image, 255, offset = 10)
+        threshold_image = threshold_adaptive(gaussian_image, 255, offset = 9)
         threshold_average_image = threshold_image.astype(np.uint8) * 255
-        display(threshold_average_image,'threshold_average_image')
+        display(threshold_average_image,'threshold_average_image',0)
 
 
         edge_detection_image = cv2.Canny(threshold_average_image, 200, 200)
-        display(edge_detection_image,'edge_detection_image')
+        display(edge_detection_image,'edge_detection_image',0)
 
         (_,cnts,_) = cv2.findContours(edge_detection_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         cv2.drawContours(img,cnts,-1,(0,0,255),2)
-        display(img,'final')
-        # ret,th1 = cv2.threshold(img,180,255,cv2.THRESH_BINARY)
-        # display(edge_detection_image,'edge_detection_image')
-        # median = cv2.medianBlur(gaussian,9)
-        # edged = cv2.Canny(median, 30, 200)
+        display(img,'final',0)
 
-        # image, contours, hier = cv2.findContours(median, cv2.RETR_TREE,
-        #                 cv2.CHAIN_APPROX_SIMPLE)
-
-        # print 'Contours=', len(contours)
-
-        # # display(gaussian,'gaussian')
-        # # display(median,'blur')
-        # # display(edged,'edged')
-        # # display(image,'image')
-
-
-
-        # for cnt in contours:
-        #     print  'here'
-        #     epsilon = 0.1*cv2.arcLength(cnt,True)
-        #     approx = cv2.approxPolyDP(cnt,epsilon,True)
-        #     print  'here2'
-        #     M = cv2.moments(approx)
-        #     # print( M )
-        #     area = cv2.contourArea(approx)
-        #     if area>500 :#and area <22000:
-        #         print 'area= ',area
-        #         mask = np.zeros(image.shape, dtype = "uint8")
-        #         cv2.drawContours(img, approx, -1, (255, 0, 0), -1)
-        #         display(img,'mask')
-        # display(mean, 'mean')
-        # display(gaussian,'gaussian')
         cv2.destroyAllWindows()
         i+=1
 
 
+def threshold(image):
+
+    gaussian_image = scpy.gaussian_filter(image, (10,10))
+    display(gaussian_image,'gaussian_image',0)
+
+    threshold_image = threshold_adaptive(gaussian_image, 255, offset = 10)
+    threshold_average_image = threshold_image.astype(np.uint8) * 255
+    display(threshold_average_image,'threshold_average_image',0)
+    # ret,th1 = cv2.threshold(img,180,255,cv2.THRESH_BINARY)
+
+    image, contours, hier = cv2.findContours(threshold_average_image, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)#, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    smears=[]
+
+    for cnt in contours:
+
+        area = cv2.contourArea(cnt)
+        if area>1000:# and area<7000:
+            print 'area=',area
+            smears.append(cnt)
+
+    mask = np.zeros(threshold_average_image.shape, np.uint8)
+    cv2.drawContours(mask, smears, -1, (255, 255, 255), -1)
+
+    cv2.imwrite('mask.jpg',mask)
+    print 'mask saved'
+
+    cv2.destroyAllWindows()
+    return mask
+
+
 
 def main():
-    # arguments = sys.argv[1:]
 
     #Checking if path is entered
     if len(sys.argv) < 2:
         path = '/home/kashish/Downloads/sample_drive/cam_5'
         print 'No path given, using defaut path'
-        # sys.exit(0)
+
     else:
         path =sys.argv[1]
 
     #Number of images in the folder
     n_of_images=glob.glob(path + '/*.jpg')
 
-    threshold(path)
-    sys.exit(0)
-
-
     #New approach
-    sum = subtract(path,n_of_images)
-    cv2.imshow('sum', sum)
-    cv2.waitKey(0)
+    avg_image = find_average(path,n_of_images)
+    cv2.imwrite('mask1.jpg',avg_image)
+    display( avg_image,'Average',0)
+
+    # sum =cv2.imread('/home/suhailps/Desktop/Link to Spring_18/Geospatial/Assignment1/Smear_detection/cam14.png',0)
+
+    #Function outputs the mask of the smear
+    mask=threshold(avg_image)
+    display( mask,'Mask',0)
+
     cv2.destroyAllWindows()
 
-    sys.exit(1)
-    #New approach
-
-    #Finding the average image and
-    avg_image=average_images(path,n_of_images)
-    display(avg_image,'Average image')#Display the average image
-
-    #Applying Gaussian Blur
-    avg_blurred = cv2.GaussianBlur(avg_image, (7,7),0)
-    display(avg_blurred,'Blurred Image')
-
-    clahe = cv2.createCLAHE(clipLimit=20.0, tileGridSize=(6,6))
-    cl1 = clahe.apply(avg_blurred)
-    display(cl1,'After CLAHE'
-)
-    ret,th1 = cv2.threshold(cl1, 50,255,cv2.THRESH_BINARY)
-    display(th1,'Threshold img')
-
-
-    image, contours, hier = cv2.findContours(th1, cv2.RETR_TREE,
-                    cv2.CHAIN_APPROX_SIMPLE)
-
-    cv2.drawContours(image, contours, -1, (255,0,0), 10)
-    display(image,'Contours')
-
-    print 'Number of contours= ',len(contours)
-
-    # with each contour, draw boundingRect in green
-    # a minAreaRect in red and
-    # a minEnclosingCircle in blue
-    for c in contours:
-        # get the bounding rect
-        x, y, w, h = cv2.boundingRect(c)
-        # draw a green rectangle to visualize the bounding rect
-        if x!=0 and y!=0:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-            # get the min area rect
-            rect = cv2.minAreaRect(c)
-            box = cv2.boxPoints(rect)
-            # convert all coordinates floating point values to int
-            box = np.int0(box)
-            # draw a red 'nghien' rectangle
-            cv2.drawContours(img, [box], 0, (0, 0, 255))
-
-
-    #     else:
-    #         contours.remove(c)
 
 if __name__=="__main__":
     main()
+    # threshold2(path)
+    # sys.exit(0)
+
